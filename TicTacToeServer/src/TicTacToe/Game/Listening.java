b5b5f84f3ead7476dc69d62302827ee90d305fbe
aassad1 <159203159+aassad1@users.noreply.github.com>
2024-05-16 18:30:54 -0700

    listening file made more readable, blocks of code easier to follow now -Ali

--- TicTacToeServer/src/TicTacToe/Game/Listening.java
+++ TicTacToeServer/src/TicTacToe/Game/Listening.java
@@ -23,83 +23,97 @@ public Listening(Socket socket)
         this.player = player;
         System.out.println("Listening on Player" + player);
     }
+
     public void setGameData(GameData gameData)
     {
         this.gameData = gameData;
     }
 
     @Override
-    public void run() {
-        try {
+    public void run()
+    {
+        try
+        {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println("Listening on Player" + player);
 
-        while (!shutdown) // make sockets trade off by move
-        {
-            System.out.println("the socket is closed -- " + socket.isClosed());
-            System.out.println("reading in data for Player " + player);
-            message = in.readLine();
-            System.out.println("read in data for Player " + player);
-
-            if (gameData != null)
+            while (!shutdown) // make sockets trade off by move
             {
-                if (message == null) {
-                System.out.println("no message");
-                    if (socket.isClosed()) {
-                        System.out.println("socket is closed");
-                        shutdown();
+                System.out.println("the socket is closed -- " + socket.isClosed());
+                System.out.println("reading in data for Player " + player);
+                message = in.readLine();
+                System.out.println("read in data for Player " + player);
+
+                if (gameData != null)
+                {
+                    if (message == null)
+                    {
+                        System.out.println("no message");
+                        if (socket.isClosed())
+                        {
+                            System.out.println("socket is closed");
+                            shutdown();
+                        }
+                        //continue;
                     }
-                    continue;
-                } else if (message.contains("/move")) {
-                    System.out.println("moving " + message);
-                    turn(String.valueOf(message.charAt(message.length() - 1)));
-                } else if (message.contains("/quit")) {
-                    if (!gameData.isShutdown()) {
-                        gameData.alert("/quit" + player);
+                    else if (message.contains("/move"))
+                    {
+                        System.out.println("moving " + message);
+                        turn(String.valueOf(message.charAt(message.length() - 1)));
                     }
-                    shutdown = true;
-                } else if (message.contains("/serverShutdown"))
-                {
-                    System.out.println("Server shutting down message recieved");
-                    shutdown = true;
-                }
-                else if (message.contains("/difficulty")) {
-                    gameData.alert(message);
-                }
-                else if (message.contains("/clearBoard"))
-                {
-                    System.out.println("clear board message received");
-                    gameData.alert(message);
-                }
-                else if (message.contains("/switchSides"))
-                {
-                    System.out.println("switch sides message received");
-                    gameData.alert(message);
-                }
-                else if (message.contains("/message"))
-                {
-                    System.out.println("chat message received");
-                    gameData.alert(message);
-                }
+                    else if (message.contains("/quit"))
+                    {
+                        if (!gameData.isShutdown())
+                        {
+                            gameData.alert("/quit" + player);
+                        }
+                        shutdown = true;
+                    }
+                    else if (message.contains("/serverShutdown"))
+                    {
+                        System.out.println("Server shutting down message recieved");
+                        shutdown = true;
+                    }
+                    else if (message.contains("/difficulty")) {
+                        gameData.alert(message);
+                    }
+                    else if (message.contains("/clearBoard"))
+                    {
+                        System.out.println("clear board message received");
+                        gameData.alert(message);
+                    }
+                    else if (message.contains("/switchSides"))
+                    {
+                        System.out.println("switch sides message received");
+                        gameData.alert(message);
+                    }
+                    else if (message.contains("/message"))
+                    {
+                        System.out.println("chat message received");
+                        gameData.alert(message);
+                    }
+                    else
+                    {
+                        System.out.println("unknown message received");
+                    }
+
+                } //end of large if block
+
                 else
                 {
-                    System.out.println("unknown message received");
-                    continue;}
-            }
-            else
-            {
-                System.out.println("Game data null");
-                if (message.contains("/quit"))
-                {
-                    EstablishGame.removeSocket(socket, this);
-                    Main.removeSocket(socket);
-                    shutdown = true;
-                }
-            }
+                    System.out.println("Game data null");
+                    if (message.contains("/quit"))
+                    {
+                        EstablishGame.removeSocket(socket, this);
+                        Main.removeSocket(socket);
+                        shutdown = true;
+                    }
+                }//end else
 
-        }
+            } //end while
 
-        } catch (Exception e) {
+        } //end try block
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
         finally
@@ -108,6 +122,8 @@ public Listening(Socket socket)
         }
         System.out.println("Shutting down listener for player" + player);
     }
+
+
     private void turn(String move) throws IOException
     {
         boolean valid = true;
